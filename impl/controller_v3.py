60ad474e51c9aa432919663fa70f6a9815952477
m23 <maxmaxeltek@gmail.com>
2025-06-09 14:24:43 +0200

    gif viewer test

--- impl/controller_v3.py
+++ impl/controller_v3.py
@@ -98,9 +98,9 @@ def main():
     app_list = [
                     main_screen.MainScreen(config, modules, callbacks),
                     # notion_v2.NotionScreen(config, modules, callbacks),
-                    weather.WeatherScreen(config, modules, callbacks),
+                    # weather.WeatherScreen(config, modules, callbacks),
                     # subcount.SubcountScreen(config, modules, callbacks),
-                    # gif_viewer.GifScreen(config, modules, callbacks),
+                    gif_viewer.GifScreen(config, modules, callbacks),
                     #life.GameOfLifeScreen(config, modules, callbacks)
                 ]
 
